5dd37ca98cae3bc72f5bd930dee9daf12eb750a4
Owen Evans <owen@3sidedcube.com>
2022-02-07 15:53:06 +0000

    app folder linted and passes

--- app/src/services/mailService.js
+++ app/src/services/mailService.js
@@ -1,32 +1,30 @@
-const logger = require('logger');
-const config = require('config');
-const SparkPost = require('sparkpost');
+const logger = require("logger");
+const config = require("config");
+const SparkPost = require("sparkpost");
 
-const sendMailThunk = (client, reqOpts) => (callback) => {
-    client.transmissions.send(reqOpts, callback);
+const sendMailThunk = (client, reqOpts) => callback => {
+  client.transmissions.send(reqOpts, callback);
 };
 
 class MailService {
+  constructor() {
+    this.client = new SparkPost(config.get("sparkpost.apikey"));
+  }
 
-    constructor() {
-        this.client = new SparkPost(config.get('sparkpost.apikey'));
-    }
+  *sendMail(template, data, recipients) {
+    logger.info("Sending email with template %s to recipients: ", template, recipients);
+    const reqOpts = {
+      substitution_data: data,
+      content: {
+        template_id: template
+      },
+      recipients
+    };
 
-    * sendMail(template, data, recipients) {
-        logger.info('Sending email with template %s to recipients: ', template, recipients);
-        const reqOpts = {
-            substitution_data: data,
-            content: {
-                template_id: template
-            },
-            recipients
-        };
-
-        const response = yield sendMailThunk(this.client, reqOpts);
-        logger.debug('Email sent correctly!!');
-        return response;
-    }
-
+    const response = yield sendMailThunk(this.client, reqOpts);
+    logger.debug("Email sent correctly!!");
+    return response;
+  }
 }
 
 module.exports = new MailService();
